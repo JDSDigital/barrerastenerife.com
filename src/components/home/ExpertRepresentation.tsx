9bc485ca4658946710014e8f37cd47a220dd8cd9
Jorge Daniel Sosa <jorgesosa@acidtango.com>
2023-07-30 18:02:48 +0100

    Fix: Production fixes

--- src/components/home/ExpertRepresentation.tsx
+++ src/components/home/ExpertRepresentation.tsx
@@ -1,10 +1,10 @@
-import { Button, Container, Grid, Typography, styled } from "@material-ui/core";
-import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import { Container, Grid, Typography } from "@material-ui/core";
 import { graphql, useStaticQuery } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 import { Link } from "gatsby-plugin-react-i18next";
-import React from "react";
 import { useTranslation } from "hooks/useTranslation";
+import React from "react";
 
 export const ExpertRepresentation = () => {
   const data = useStaticQuery(graphql`
@@ -70,15 +70,11 @@ export const ExpertRepresentation = () => {
           </div>
         </Grid>
         <Grid item xs={12} sm={6} data-aos="fade-left" data-aos-delay="400">
-          <ImageContainer>
+          <div className="expert-image-container">
             <GatsbyImage image={image} alt="" className="expert-image" />
-          </ImageContainer>
+          </div>
         </Grid>
       </Grid>
     </Container>
   );
 };
-
-const ImageContainer = styled("div")({
-  height: "600px",
-});
