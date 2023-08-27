d4916f692438d763e380031ec171b0f118c940a3
Jorge Daniel Sosa <jorgesosa@acidtango.com>
2023-08-27 14:49:29 +0100

    Chore: Expert representation text

--- src/components/home/ExpertRepresentation.tsx
+++ src/components/home/ExpertRepresentation.tsx
@@ -2,7 +2,7 @@ import { Container, Grid, Typography } from "@material-ui/core";
 import { graphql, useStaticQuery } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
-import { Link } from "gatsby-plugin-react-i18next";
+import { Link, Trans } from "gatsby-plugin-react-i18next";
 import { useTranslation } from "hooks/useTranslation";
 import React from "react";
 
@@ -36,19 +36,7 @@ export const ExpertRepresentation = () => {
         <Grid container spacing={5}>
           <Grid item xs={12} sm={6} data-aos="fade-right" data-aos-delay="400">
             <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-              luctus orci at nunc lacinia, condimentum molestie lectus
-              hendrerit. Praesent fringilla massa a semper aliquam. Lorem ipsum
-              dolor sit amet, consectetur adipiscing elit. Fusce sit amet
-              elementum nulla. Nunc sagittis lacinia tempus. Praesent mollis non
-              quam in rhoncus. In sodales lacus mattis luctus semper. Sed
-              vehicula nibh at quam porta elementum. Aliquam sagittis tempor
-              metus at consequat. Curabitur quis sem quis velit mattis eleifend
-              eget eget enim. Praesent orci ex, congue malesuada porttitor ac,
-              viverra eget velit. Vivamus at quam nec nulla hendrerit malesuada.
-              Pellentesque bibendum tincidunt orci, id tincidunt elit mollis
-              quis. Sed iaculis lacinia consectetur. Nulla feugiat tincidunt
-              convallis. Integer at porta est, in blandit enim.
+              <Trans i18nKey="expertRepresentation.description" />
             </Typography>
             <div className="button-container">
               <div className="MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-containedSizeLarge MuiButton-sizeLarge">
